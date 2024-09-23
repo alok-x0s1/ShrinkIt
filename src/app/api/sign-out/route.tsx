912ba7934556cffd9ccd732ef9b294478a2bbf51
Alok Yadav <yalok6321@gmail.com>
2024-09-23 20:54:30 +0530

    api to logout user

---
+++ src/app/api/sign-out/route.tsx
@@ -0,0 +1,76 @@
+import {
+	createErrorResponse,
+	createSuccessResponse,
+} from "@/helpers/ApiResponse";
+import { dbConnect } from "@/lib/dbConnect";
+import User from "@/models/userModel";
+import { RefreshTokenPayload } from "@/types/jwtTypes";
+import { accessCookieOptions, refreshCookieOptions } from "@/utils/constants";
+import { verifyToken } from "@/utils/verifyToken";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+	await dbConnect();
+
+	try {
+		const incomingAccessToken = req.cookies.get("accessToken")?.value;
+		const incomingRefreshToken = req.cookies.get("refreshToken")?.value;
+
+		if (!incomingAccessToken || !incomingRefreshToken) {
+			return NextResponse.json(
+				createErrorResponse("You are already signed out."),
+				{
+					status: 401,
+				}
+			);
+		}
+
+		// if (!incomingAccessToken && incomingRefreshToken) {
+		// 	return NextResponse.json(
+		// 		createErrorResponse(
+		// 			"You don't have access token. Please refresh your toekn."
+		// 		),
+		// 		{
+		// 			status: 401,
+		// 		}
+		// 	);
+		// }
+
+		const decodedRefreshToken = verifyToken<RefreshTokenPayload>(
+			incomingRefreshToken,
+			process.env.REFRESH_TOKEN_SECRET ?? ""
+		);
+
+		const user = await User.findOne({ _id: decodedRefreshToken.id });
+
+		if (!user) {
+			return NextResponse.json(createErrorResponse("User not found."), {
+				status: 404,
+			});
+		}
+
+		await user.updateOne({ $set: { refreshToken: null } });
+
+		const response = NextResponse.json(
+			createSuccessResponse("Signed out successfully"),
+			{
+				status: 200,
+			}
+		);
+		response.cookies.set("accessToken", "", accessCookieOptions);
+		response.cookies.set("refreshToken", "", refreshCookieOptions);
+
+		return response;
+	} catch (error) {
+		console.error("Sign-out error:", error);
+		return NextResponse.json(
+			createErrorResponse(
+				"Failed to sign out",
+				error instanceof Error ? error.message : "Unknown error"
+			),
+			{
+				status: 500,
+			}
+		);
+	}
+}
