e0b8f1af302bb67a27275b7b9725a263daa956b6
Alok Yadav <yalok6321@gmail.com>
2024-09-29 23:03:26 +0530

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,11 +49,6 @@ const config: Config = {
     				'3': 'hsl(var(--chart-3))',
     				'4': 'hsl(var(--chart-4))',
     				'5': 'hsl(var(--chart-5))',
-    				'1': 'hsl(var(--chart-1))',
-    				'2': 'hsl(var(--chart-2))',
-    				'3': 'hsl(var(--chart-3))',
-    				'4': 'hsl(var(--chart-4))',
-    				'5': 'hsl(var(--chart-5))'
     			},
     			green: {
     				'500': '#24AE7C',
